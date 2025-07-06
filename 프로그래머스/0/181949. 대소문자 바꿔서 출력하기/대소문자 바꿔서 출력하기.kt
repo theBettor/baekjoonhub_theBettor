bf1688cdff32e2857f8bf5f1b6a2621eba6588ae
Bettor <be0128st@gmail.com>
2025-07-07 00:26:47 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 170.14 ms, Memory: 50 MB -BaekjoonHub

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.kt
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.kt
@@ -8,4 +8,13 @@ fun main(args: Array<String>) {
         }
     }.joinToString("")
     println(result)
+    
+    // for(i in s1.indices){
+    //     if(s1[i].isUpperCase()){
+    //         print(s1[i].toLowerCase())
+    //     }else{
+    //         print(s1[i].toUpperCase())
+    //     }
+    // }
 }
+
