27c76b0dfad8c0f01ccd21f3bafd00a4fc2f9ef8
Bettor <be0128st@gmail.com>
2025-07-07 21:01:47 +0900

    [level 0] Title: 할 일 목록, Time: 14.26 ms, Memory: 64.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(todo_list: Array<String>, finished: BooleanArray): Array<String> {
+        var result = arrayOf<String>()
+        todo_list.zip(finished.toTypedArray()) { todo, finished ->
+            if (!finished) result += todo
+        }
+        return result
+    }
+}
+
+// 문자열 배열 결과를 담기 위한 result 변수를 만들고, zip으로 두 배열을 합치기 위해 BooleanArray를 toTypedArray()를 사용해 Array<Boolean>으로 변환한다. 이렇게 두 배열을 병합한 뒤, 각 요소 쌍 (todo, finished)를 람다식으로 순회하면서 finished가 false인 경우 result에 todo를 추가하는 방식으로 동작하며, zip은 결과를 반환하지 않기 때문에 함수의 최종 반환값인 result를 명시적으로 return 해준다.
