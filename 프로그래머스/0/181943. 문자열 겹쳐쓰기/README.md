81e9300dfa6dbcb4ed68dbbf89eb5cce46b6fab4
Bettor <be0128st@gmail.com>
2025-07-07 18:11:13 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 10.17 ms, Memory: 64.1 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/README.md
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 겹쳐쓰기 - 181943 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181943) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181943?language=kotlin) 
 
 ### 성능 요약
 
-메모리: 64.5 MB, 시간: 14.53 ms
+메모리: 64.1 MB, 시간: 10.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 07일 18:06:16
+2025년 07월 07일 18:11:11
 
 ### 문제 설명
 
--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.kt
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.kt
@@ -1,6 +1,8 @@
 class Solution {
-    fun solution(my_string: String, overwrite_string: String, s: Int): String
-    = my_string.substring(0, s) + overwrite_string + my_string.substring(s + overwrite_string.length)
-    // 인덱스 s부터 overwirte_string의 길이만큼을 overrite_string에 겹쳐쓰는 것이므로
-    // substring으로 0부터 s까지 자르고 문자열 겹쳐서 이어주고, s부터 overwrite_string.length만큼도 붙혀준다.
+    // fun solution(my_string: String, overwrite_string: String, s: Int): String
+    // = my_string.substring(0, s) + overwrite_string + my_string.substring(s + overwrite_string.length)
+    // // 인덱스 s부터 overwirte_string의 길이만큼을 overrite_string에 겹쳐쓰는 것이므로
+    // // substring으로 0부터 s까지 자르고 문자열 겹쳐서 이어주고, s부터 overwrite_string.length만큼도 붙혀준다.
+    fun solution(myString: String, overwriteString: String, s: Int): String =
+        myString.replaceRange(s, s + overwriteString.length, overwriteString)
 }
